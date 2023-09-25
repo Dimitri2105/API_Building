089999dfd043b072a2d7d7320306868c3addeae4
Dimitri2105 <karanthakur577@gmail.com>
2023-09-25 11:34:19 +0530

    post route for states  created

--- modals/stateModal.js
+++ modals/stateModal.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const stateModal = new Schema({
-    stateName : {
+    statename : {
         type : String,
         required : true
     }
--- route/routes.js
+++ route/routes.js
@@ -84,6 +84,38 @@ Router.get('/API/get-state' , async(req,res,next) =>{
   }
 })
 
+Router.post('/API/create-state' , async (req,res,next) =>{
+  try{
+
+    const { statename } = req.body;
+
+    if(!statename){
+      return res.status(200).json({message : "State Name absent"})
+    }
+
+    const existingState = await State.findOne({ statename });
+
+  if (existingState) {
+    return res.status(400).json({ message: "State Name already exists" });
+  }
+    const state = new State({
+      statename
+    })
+
+    await state.save()
+
+    res.status(200).json({
+      message : "Success",
+      statename : state
+    })
+
+
+  }
+  catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error });
+  }
+})
 
 
 module.exports = Router;

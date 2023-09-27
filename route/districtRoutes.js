6bc7cf109cd9eaf33a368f71e7c6b5ba73740471
Tarathakur@21 <karanthakur577@gmail.com>
2023-09-27 15:19:12 +0530

    Middleware for user authentication

--- route/districtRoutes.js
+++ route/districtRoutes.js
@@ -1,33 +1,18 @@
-const express = require("express");
+const express = require('express')
 
-const districtController = require("../controllers/districtController");
+const districtController = require('../controllers/districtController')
 
-const authentication = require("../middleware/auth");
+const authentication = require('../middleware/auth')
 
-const Router = express.Router();
 
-Router.get(
-  "/API/get-district",
-  authentication.authenticate,
-  districtController.getDistricts
-);
+const Router = express.Router()
 
-Router.post(
-  "/API/create-district",
-  authentication.authenticate,
-  districtController.createDistrict
-);
+Router.get("/API/get-district",authentication.authenticate,districtController.getDistricts);
+  
+Router.post("/API/create-district", authentication.authenticate, districtController.createDistrict);
 
-Router.post(
-  "/API/update-district",
-  authentication.authenticate,
-  districtController.updateDistrict
-);
+Router.post('/API/update-district' ,  authentication.authenticate, districtController.updateDistrict)
 
-Router.post(
-  "/API/remove-district",
-  authentication.authenticate,
-  districtController.removeDistrict
-);
+Router.post('/API/remove-district' ,authentication.authenticate, districtController.removeDistrict)
 
-module.exports = Router;
+module.exports = Router
--- route/stateRoutes.js
+++ route/stateRoutes.js
@@ -1,33 +1,17 @@
-const express = require("express");
+const express = require('express')
 
-const stateController = require("../controllers/stateController");
-const userValidation = require("../middleware/userValidation");
-const authentication = require("../middleware/auth");
+const stateController = require('../controllers/stateController')
+const userValidation = require('../middleware/userValidation')
+const authentication = require('../middleware/auth')
 
-const Router = express.Router();
+const Router = express.Router()
 
-Router.get(
-  "/API/get-state",
-  authentication.authenticate,
-  stateController.getStates
-);
+Router.get("/API/get-state",authentication.authenticate,stateController.getStates);
+  
+Router.post("/API/create-state",authentication.authenticate,stateController.createState );
 
-Router.post(
-  "/API/create-state",
-  authentication.authenticate,
-  stateController.createState
-);
+Router.post("/API/update-state" ,authentication.authenticate, stateController.updateState)
 
-Router.post(
-  "/API/update-state",
-  authentication.authenticate,
-  stateController.updateState
-);
-
-Router.post(
-  "/API/remove-state",
-  authentication.authenticate,
-  stateController.removeState
-);
+Router.post("/API/remove-state" ,authentication.authenticate, stateController.removeState)
 
 module.exports = Router;
